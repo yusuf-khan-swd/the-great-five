ee89a739f810c1bad8308f2f30a8c8fd8b436599
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-08-22 00:48:39 +0600

    add messi section and calculate player expenses and calculate total cost

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 
+// Select Messi
+document.getElementById('select-messi').addEventListener('click', function() {
+
+  document.getElementById('select-messi').setAttribute('disabled', true)
+
+  const playerName = document.getElementById('messi').innerText;
+  const parentElement = document.getElementById('selected-players-list');
+
+  const li = document.createElement('li');
+  li.innerText = playerName;
+  parentElement.appendChild(li);
+});
+
+// Calculate PlayerExpenses
+document.getElementById('calculate').addEventListener('click', function() {
+  const perPlayerCostField = document.getElementById('per-player-cost');
+  const perPlayerCostString = perPlayerCostField.value;
+  const perPlayerCost = parseFloat(perPlayerCostString);
+
+  const selectedPlayerList = document.getElementById('selected-players-list').children;
+  const selectedPlayers = selectedPlayerList.length;
+
+  const playerExpenses = perPlayerCost * selectedPlayers;
+  document.getElementById('player-expenses').innerText = playerExpenses;
+});
+
+// Calculate total Cost 
+document.getElementById('calculate-total').addEventListener('click', function() {
+  const playerExpensesString = document.getElementById('player-expenses').innerText;
+  const playerExpenses = parseFloat(playerExpensesString);
+
+  const coachCostString = document.getElementById('coach-cost').value;
+  const coachCost = parseFloat(coachCostString);
+
+  const managerCostString = document.getElementById('manager-cost').value;
+  const managerCost = parseFloat(managerCostString);
+
+  const total = playerExpenses + coachCost + managerCost;
+  document.getElementById('total').innerText = total;
+
+});
