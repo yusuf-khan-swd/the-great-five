0be8fed7ab373adc8c60e95a9dfd967a7c830636
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-08-22 13:11:34 +0600

    create a function to get element innerText and calculate total cost with functions

--- js/calculations.js
+++ js/calculations.js
@@ -6,6 +6,11 @@ function getInputValue(inputId) {
   return inputValue;
 }
 
+function getElementInnerText(elementId) {
+  const element = document.getElementById(elementId);
+  return parseFloat(element.innerText);
+}
+
 function setElementInnerText(elementId, valueToSet) {
   const element = document.getElementById(elementId);
   element.innerText = valueToSet;
@@ -18,23 +23,19 @@ document.getElementById('calculate').addEventListener('click', function() {
   const selectedPlayerList = document.getElementById('selected-players-list').children;
   const selectedPlayers = selectedPlayerList.length;
   
-  const playerExpenses = perPlayerCost * selectedPlayers;
-  
+  const playerExpenses = perPlayerCost * selectedPlayers;  
   setElementInnerText('player-expenses', playerExpenses)
 });
 
 // Calculate total Cost 
 document.getElementById('calculate-total').addEventListener('click', function() {
-  const playerExpensesString = document.getElementById('player-expenses').innerText;
-  const playerExpenses = parseFloat(playerExpensesString);
+  const playerExpenses = getElementInnerText('player-expenses');
 
-  const coachCostString = document.getElementById('coach-cost').value;
-  const coachCost = parseFloat(coachCostString);
+  const coachCost = getInputValue('coach-cost');
 
-  const managerCostString = document.getElementById('manager-cost').value;
-  const managerCost = parseFloat(managerCostString);
+  const managerCost = getInputValue('manager-cost');
 
   const total = playerExpenses + coachCost + managerCost;
-  document.getElementById('total').innerText = total;
+  setElementInnerText('total', total);
 
 });
